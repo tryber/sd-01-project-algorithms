0f74a08e345e260fcac0c0c51aa7424255e9ab36
henriqueeyer <henriqueeyer@outlook.com>
2020-09-11 14:57:12 -0300

    foi

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -9,15 +9,6 @@ def is_anagram(first_string, second_string):
         if not ok:
             return ok
     return True
-
-
-def is_anagram2(first_string, second_string):
-    # Faça o código aqui.
-    for i in range(len(first_string)):
-        
-
-
-
 # first_string = "pedra"
 # second_string = "perda"
 
@@ -25,11 +16,46 @@ def is_anagram2(first_string, second_string):
 first_string = "pato"
 second_string = "tapo"
 
-
 # first_string = "coxinha"
 # second_string = "empada"
 
 
-print(is_anagram(first_string, second_string))
+def merge_sort(value):
+    array = list(value)
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+    return merge(left, right, array.copy())
 
 
+def merge(left, right, merged):
+
+    left_cursor, right_cursor = 0, 0
+
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
+
+def is_anagram2(first_string, second_string):
+    order_first_string = merge_sort(first_string)
+    order_second_string = merge_sort(second_string)
+    print(order_first_string)
+    print(order_second_string)
+    return order_first_string == order_second_string
+
+
+print(is_anagram2(first_string, second_string))
