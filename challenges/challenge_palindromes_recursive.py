aed08b5ea4137d031d3a8b96b8e4bb759d9c711d
dougfunny <douglaseduardo220483@gmail.com>
2020-09-14 16:54:47 -0300

    Fazendo o palindromo recursivo com apenas 1 retorno

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,19 +1,18 @@
-def is_palindrome_recursive(word: str, index: int = 0) -> bool:
-    if len(word) == 0:
-        return False
-    if index == len(word) // 2:
-        return True
-    if word[index] == word[(index + 1) * -1]:
-        return is_palindrome_recursive(word, index + 1)
-    return False
+# def is_palindrome_recursive(word: str, index: int = 0) -> bool:
+#     if len(word) == 0:
+#         return False
+#     if index == len(word) // 2:
+#         return True
+#     if word[index] == word[(index + 1) * -1]:
+#         return is_palindrome_recursive(word, index + 1)
+#     return False
 
 
-# def is_palindrome_recursive(word):
-#     return (
-#         is_palindrome_recursive(word[1:-1]) and False
-#         if (word == "")
-#         else (True if len(word) < 2 or (word[0] == word[-1]) else False)
-#     )
-
-
-# print(is_palindrome_recursive("SOCOS"))
+def is_palindrome_recursive(word):
+    return (
+        False
+        if word == ""
+        else len(word) < 2
+        or word[0] == word[-1]
+        and is_palindrome_recursive(word[1:-1])
+    )
