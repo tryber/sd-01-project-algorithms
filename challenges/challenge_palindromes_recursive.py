5e0c117c721e605d8b6813664515f5d587682dbf
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-09-20 22:50:38 -0300

    finalizando requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,16 @@
 def is_palindrome_recursive(word, low, high):
-    # Faça o código aqui.
+    if word == "":
+        return False
+
+    if low >= (len(word) // 2):
+        return f"{word} é um palídromo"
+
+    character_count = low + 1
+
+    if word[low] == word[high]:
+        return is_palindrome_recursive(word, character_count, len(word) - (1 + character_count))
+    else:
+        return f"{word} não é um palídromo"
 
 
 word = ""
