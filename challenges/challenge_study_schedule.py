d0e11e9e2cc8d4ca2e4fc36bfb39ac0aa798eac0
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-09-20 22:16:08 -0300

    Finalizando requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,16 @@
 def study_schedule(start_time, end_time, target_time):
-    # Faça o código aqui.
+    best_time = 0
+    if target_time == 0:
+        return f"O valor do target_time não pode ser {target_time}"
+
+    for index in range(len(start_time)):
+        if start_time[index] <= target_time <= end_time[index]:
+            best_time += 1
+
+    return f"A quantidade de estudantes que se adaptam a este horário é: {best_time}"
 
 
-start_time = []
-end_time = []
-target_time = 0
+start_time = [2, 1, 2, 1, 4, 4]
+end_time = [2, 2, 3, 5, 5, 5]
+target_time = 2
 print(study_schedule(start_time, end_time, target_time))
