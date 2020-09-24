a05ed455033b8b41e201c3a14d2b15cc0519659b
Conrado Medeiros <conrado.medeiros@localiza.com>
2020-09-24 17:41:39 -0300

    challenge study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,11 @@
 def study_schedule(start_time, end_time, target_time):
+    if target_time == 0:
+        return False
+    students_qtd = 0
+    for index in range(len(start_time)):
+        if start_time[index] <= target_time and target_time <= end_time[index]:
+            students_qtd += 1
+    return students_qtd
 
 
 start_time = []
